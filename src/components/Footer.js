5118b410d83f69219b09e3f0c4472ec8b5792672
Beknur Maxalbayev <120553658+beknurmaxalbayev@users.noreply.github.com>
2022-12-27 11:30:55 +0500

    Bek Calculator

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+export default function Footer() {
+    var stringCode = "</>"
+    return (
+        <footer>
+            <p> {stringCode} by: <a href="https://qwasar.io//">Bek Calculator</a> </p>
+            <div className="qarr">
+                </div>
+                        </footer>
+    )
+}
